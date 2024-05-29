6d28fb26ec33e2e0282379e123d4f817d506e162
jingt <jingt.uws@gmail.com>
2024-05-29 11:56:07 -0700

    remove hidden textr

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const button = document.getElementById('myButton');
-button.addEventListener("click", nextPage);
+// button.addEventListener("click", nextPage);
 let page1 = document.getElementById("page1")
 let page2 = document.getElementById("page2")
-page1.style.display = "block";
-page2.style.display = "none";
+// page1.style.display = "block";
+// page2.style.display = "none";
 
 function nextPage() {
     // if its not hidden, hide it
