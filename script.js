ee46f9653cc0e380ff299eabc33d081f10d5909e
jingt <jingt.uws@gmail.com>
2024-06-10 15:02:39 -0700

    change file structure, add new reading

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-const button = document.getElementById('myButton');
-// button.addEventListener("click", nextPage);
-let page1 = document.getElementById("page1")
-let page2 = document.getElementById("page2")
-// page1.style.display = "block";
-// page2.style.display = "none";
-
-function nextPage() {
-    // if its not hidden, hide it
-    if (page1.style.display !== "none") {
-        page1.style.display = "none";
-    } else {
-        page1.style.display = "block";
-    }
-    if (page2.style.display === "none") {
-        page2.style.display = "block";
-    } else {
-        page2.style.display = "none";
-    }
-}
-
+const title = document.querySelector('h1');
+title.addEventListener('click', () => {
+    // redirect to index.html
+    window.location.href = '/index.html';
+});
